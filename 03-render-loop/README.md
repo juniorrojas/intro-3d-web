34fc771bcdb613a1cb07007d0ff0edd2071d1ea2
Junior Rojas <jrojasdavalos@gmail.com>
2020-08-13 20:18:31 -0600

    Update 03-render-loop

--- 03-render-loop/README.md
+++ 03-render-loop/README.md
@@ -1,6 +1,6 @@
-03-render-loop
-======
-We will add code to animate the box with a constant angular speed.
+# 03-render-loop
+
+Animated box with a constant angular speed.
 
 [Live demo](https://juniorrojas.github.io/intro-3d-web/03-render-loop)
 
--- 03-render-loop/main.js
+++ 03-render-loop/main.js
@@ -1,47 +1,42 @@
-var scene;
-var camera;
-var renderer;
-var box;
-
 function init() {
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(75, 1, 1, 10000);
+  window.scene = new THREE.Scene();
+  window.camera = new THREE.PerspectiveCamera(75, 1, 1, 10000);
 
-	renderer = new THREE.WebGLRenderer();
-	var canvas = renderer.domElement;
-	document.body.appendChild(canvas);
+  window.renderer = new THREE.WebGLRenderer();
+  const canvas = renderer.domElement;
+  document.body.appendChild(canvas);
 
-	box = addBox();
+  window.box = addBox();
 
-	window.addEventListener("resize", window_onResize);
-	window_onResize();
-	renderLoop();
+  window.addEventListener("resize", onWindowResize);
+  onWindowResize();
+  renderLoop();
 }
 
 function addBox() {
-	var geometry = new THREE.BoxGeometry(30, 30, 30);
-	var material = new THREE.MeshBasicMaterial({
-		color: 0xff0000,
-		wireframe: true
-	});
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.z = -100;
-	mesh.rotation.y = 1;
-	scene.add(mesh);
-	return mesh;
+  const geometry = new THREE.BoxGeometry(30, 30, 30);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = -100;
+  mesh.rotation.y = 1;
+  scene.add(mesh);
+  return mesh;
 }
 
-function window_onResize() {
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.render(scene, camera);
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
 }
 
 function renderLoop() {
-	box.rotation.y += 0.05;
-	renderer.render(scene, camera);
-	requestAnimationFrame(renderLoop);
+  box.rotation.y += 0.05;
+  renderer.render(scene, camera);
+  requestAnimationFrame(renderLoop);
 }
 
 init();
