f8126a26c9f69b8d954f276767df3ae7bd2674ef
Junior Rojas <jrojasdavalos@gmail.com>
2020-08-13 19:52:12 -0600

    Reformat code

--- 00-empty-scene/main.js
+++ 00-empty-scene/main.js
@@ -1,10 +1,6 @@
-var scene;
-var camera;
-var renderer;
-
 function init() {
-	var width = 500;
-	var height = 400;
+	const width = 500;
+	const height = 400;
 
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
