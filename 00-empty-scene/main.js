b82043aab437455f813dd7362dc814e5fd58a6bc
Junior Rojas <jrojasdavalos@gmail.com>
2020-08-13 20:48:46 -0600

    Update 00-empty-scene

--- 00-empty-scene/main.js
+++ 00-empty-scene/main.js
@@ -2,9 +2,9 @@ function init() {
   const width = 500;
   const height = 400;
 
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
-  renderer = new THREE.WebGLRenderer();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
+  const renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
   renderer.render(scene, camera);
